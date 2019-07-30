84f93c5b7f43b44a514412fd44d6417b3468060a
Philip Kubin <kubin@live.com>
2019-07-30 02:10:32 -0400

    Update Org.js

--- src/scripts/classes/Org.js
+++ src/scripts/classes/Org.js
@@ -2,7 +2,7 @@ const Cloudant = require('@cloudant/cloudant');
 const cloudant = Cloudant({
     account:"ServiceId-3433c9c5-2f5e-46a5-8a60-f899ff657dac",
     key:"froldedsteatchadedintarn",
-    password:"db24ed72b829a14c9b79c4847cb566d2f6e1e422",
+    password:"",
     url:"https://a2777322-7e2f-4f00-88ff-5544d827c57f-bluemix.cloudant.com"
 });
 const orgDB = cloudant.db.use('organizations');
